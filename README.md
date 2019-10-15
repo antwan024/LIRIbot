5dc3817b749c970582c9e57e5b699638f6490d5d
a.nguyen024 <a.nguyen024@outlook.com>
2019-10-15 17:31:46 -0400

    completed LIRI

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # LIRIbot
 Node.js example using the Axios, node-Spotify, OMDBI (movies info), MOment and DotEnv API's. 
+
+
+Clearly state the problem the app is trying to solve (i.e. what is it doing and why)
+  -  The app is providing a service to find musicians concerts, spotify song info, and movie info.
+Give a high-level overview of how the app is organized  
+  -  This app has a main launch file (liri.js), a file with API keys, the JSON documentation, and a .txt file for the 4th example.
+Give start-to-finish instructions on how to run the app
+  -  The app will launch through the Command Line and the third and fourth inputs (process.argv) will determine the function (song, movie, etc.)
+Include screenshots, gifs or videos of the app functioning
+Contain a link to a deployed version of the app
+Clearly list the technologies used in the app
+State your role in the app development
--- liri.js
+++ liri.js
@@ -3,31 +3,32 @@ var Spotify = require('node-spotify-api');
 var axios = require("axios");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var fs = require("fs");
 
 var choice = process.argv[2];
 
 
 //error function
-var appError = function(error) {
-    if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log("---------------Data---------------");
-        console.log(error.response.data);
-        console.log("---------------Status---------------");
-        console.log(error.response.status);
-        console.log("---------------Status---------------");
-        console.log(error.response.headers);
-    } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an object that comes back with details pertaining to the error that occurred.
-        console.log(error.request);
-    } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-    }
-    console.log(error.config);
-};
+// var appError = function(err) {
+//     if (error.response) {
+//         // The request was made and the server responded with a status code
+//         // that falls out of the range of 2xx
+//         console.log("---------------Data---------------");
+//         console.log(error.response.data);
+//         console.log("---------------Status---------------");
+//         console.log(error.response.status);
+//         console.log("---------------Status---------------");
+//         console.log(error.response.headers);
+//     } else if (error.request) {
+//         // The request was made but no response was received
+//         // `error.request` is an object that comes back with details pertaining to the error that occurred.
+//         console.log(error.request);
+//     } else {
+//         // Something happened in setting up the request that triggered an Error
+//         console.log("Error", error.message);
+//     }
+//     console.log(error.config);
+// };
 
 
 //This is where the user will get concert info based on artis input.
@@ -41,34 +42,58 @@ var concert_this = function() {
             function(response) {
                 console.log(response.data);
             })
-            .catch(appError());
+//             .catch(appError());
 };
 
 
 var spotify_song = function() {
 
-    // var song = process.argv[3];
-
-    // spotify.search({ type: 'track', query: song, function(err, data) {
-    //     if (err) {
-    //         appError();
-    //     }
-       
-    //   console.log(data); 
-    // });
-
     var song = process.argv[3];
 
     spotify.search({ type: 'track', query: song , limit: 1} , function(err, data) {
         if (err) {
-          appError();
+//           appError();
         }
        
-      console.log(JSON.stringify(data, null, 2)); 
+   	console.log(JSON.stringify(data, null, 2)); 
       });
 
     
 };
+
+var movie_this = function() {
+		
+		var movie = process.argv[3];
+	
+		axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy")
+    
+    .then( function(response) {
+        console.log(response.data);
+     })
+		//.catch(appError());
+	
+};
+
+var do_what_it_says = function() {
+	
+	fs.readFile("random.txt", "utf8", function(err, data)  {
+		
+// 		if (error) {
+// 			return console.log(error);
+// 		};
+		
+		var randomText = data.split(",");
+		process.argv[2] = randomText[0];
+		process.argv[3] = randomText[1];
+		console.log(randomText);
+		spotify_song();
+		
+		
+	});	
+	
+};
+
+
 
 
 //Here is where the app will take in the node inputs (process.argv[2] and [3]) and choose which function to take place.
@@ -80,10 +105,15 @@ switch (choice) {
         break;
 
     case "spotify-this-song":
-        spotify_song()
-
-
-
-
+        spotify_song();
+		break;
+		
+	case "movie-this":
+    	movie_this();
+		break;
+		
+	case "do-what-it-says":
+    	do_what_it_says();
+		break;
 
 };
