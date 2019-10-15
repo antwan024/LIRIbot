a0723a625cc3aa3d3b419421da11a72f91e450f4
a.nguyen525i@gmail.com <a.nguyen525i@gmail.com>
2019-10-14 23:40:41 -0600

    got the second function working

--- liri.js
+++ liri.js
@@ -4,35 +4,86 @@ var axios = require("axios");
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
+var choice = process.argv[2];
 
-if(process.argv[2]==="concert-this") {
+
+//error function
+var appError = function(error) {
+    if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+    } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+    } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+    }
+    console.log(error.config);
+};
+
+
+//This is where the user will get concert info based on artis input.
+var concert_this = function() {
 
     var artist = process.argv[3];
 
+    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+    
+        .then(
+            function(response) {
+                console.log(response.data);
+            })
+            .catch(appError());
+};
 
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp").then(
-        function(response) {
-            console.log(response.data);
-        })
-        .catch(function(error) {
-            if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-                console.log("---------------Data---------------");
-                console.log(error.response.data);
-                console.log("---------------Status---------------");
-                console.log(error.response.status);
-                console.log("---------------Status---------------");
-                console.log(error.response.headers);
-            } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                console.log(error.request);
-            } else {
-      // Something happened in setting up the request that triggered an Error
-                console.log("Error", error.message);
-            }
-            console.log(error.config);
-        });
+
+var spotify_song = function() {
+
+    // var song = process.argv[3];
+
+    // spotify.search({ type: 'track', query: song, function(err, data) {
+    //     if (err) {
+    //         appError();
+    //     }
+       
+    //   console.log(data); 
+    // });
+
+    var song = process.argv[3];
+
+    spotify.search({ type: 'track', query: song , limit: 1} , function(err, data) {
+        if (err) {
+          appError();
+        }
+       
+      console.log(JSON.stringify(data, null, 2)); 
+      });
+
+    
+};
+
+
+//Here is where the app will take in the node inputs (process.argv[2] and [3]) and choose which function to take place.
+
+switch (choice) {
+
+    case "concert-this":
+        concert_this();
+        break;
+
+    case "spotify-this-song":
+        spotify_song()
+
+
+
+
 
 };
